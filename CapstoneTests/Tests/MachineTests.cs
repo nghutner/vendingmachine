327b0cba015dcfe2244c5dc97358904a588665d3
Jeril Bills <jerilbills@gmail.com>
2023-10-06 23:31:15 -0400

    Start on Machine tests (barely)

---
+++ CapstoneTests/Tests/MachineTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capstone.Classes;
+
+
+namespace CapstoneTests.Tests
+{
+    [TestClass]
+    public class MachineTests
+    {
+        [TestMethod]
+
+        public void ReadInventoryInputHappyPaths()
+        {
+            // Arrange
+            Machine sut = new Machine();
+
+        }
+    }
+}
