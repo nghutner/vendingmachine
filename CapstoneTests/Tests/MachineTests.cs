3d2038a8e9c16d61db7bc598bbad46a84426be38
Jeril Bills <jerilbills@gmail.com>
2023-10-08 14:56:22 -0400

    Add test with lowercase item code values for AddToDictionary

--- CapstoneTests/Tests/MachineTests.cs
+++ CapstoneTests/Tests/MachineTests.cs
@@ -765,5 +765,45 @@ namespace CapstoneTests.Tests
             CollectionAssert.AreEquivalent(expectedInventoryDict, sut.Inventory);
         }
 
+        [TestMethod]
+
+        public void AddToDictionaryLowercaseItemCode()
+        {
+            // Arrange
+            Machine sut = new Machine();
+            string[] input1 = { "a1", "Potato Crisps", "3.05", "Chip" };
+            string[] input2 = { "b4", "Crunchy", "1.75", "Candy" };
+            string[] input3 = { "c1", "Cola", "1.25", "Drink" };
+            string[] input4 = { "d4", "Triplemint", "0.75", "Gum" };
+
+            Dictionary<string, Item> expectedItemDict = new Dictionary<string, Item>();
+            Dictionary<string, int> expectedInventoryDict = new Dictionary<string, int>();
+
+            Chip chipExpected = new Chip("Potato Crisps", 3.05M);
+            Candy candyExpected = new Candy("Crunchy", 1.75M);
+            Drink drinkExpected = new Drink("Cola", 1.25M);
+            Gum gumExpected = new Gum("Triplemint", 0.75M);
+
+            expectedItemDict["A1"] = chipExpected;
+            expectedItemDict["B4"] = candyExpected;
+            expectedItemDict["C1"] = drinkExpected;
+            expectedItemDict["D4"] = gumExpected;
+
+            expectedInventoryDict["Potato Crisps"] = 5;
+            expectedInventoryDict["Crunchy"] = 5;
+            expectedInventoryDict["Cola"] = 5;
+            expectedInventoryDict["Triplemint"] = 5;
+
+            // Act
+            sut.AddToDictionary(input1);
+            sut.AddToDictionary(input2);
+            sut.AddToDictionary(input3);
+            sut.AddToDictionary(input4);
+
+            // Assert
+            CollectionAssert.AreEquivalent(expectedItemDict, sut.VendingMachineItems);
+            CollectionAssert.AreEquivalent(expectedInventoryDict, sut.Inventory);
+        }
+
     }
 }
